1551b8e35ffc72413b084642c1efb44ab3f86a75
aMadWiki <169121858+aMadWiki@users.noreply.github.com>
2024-07-04 18:36:40 +0700

    Add files via upload
    tes 123

---
+++ latihan1.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title> Latihan 1 </title>
+</head>
+<body>
+    <h1> Data Produk </h1>
+    <table border="1">
+<tr>
+     <td> <rowspan="2"> Nama Produk</td>
+     <td> <colspan="5"> Canon EOS M10 Kit EF-M 15-45 MM</td>
+</tr>
+<tr>
+    <td> <rowspan="2"> Harga</td>
+    <td> <colspan="5"> Rp 4,899,000<td>
+</tr>
+<tr>
+    <td> <rowspan="2"> Fitur Produk</td>
+    <td> <colspan="5"> <rowspan="7"> 
+        Kamera Mirrorless
+        Efektifitas Pixel : 18 MP
+        Tipe Sensor :   CMOS Digic 6
+        Layar : 3.0 Inch
+        Memiliki Lampu Flash dengan Jangkauan 5 Meter </td>
+</tr>
+</table>
+</body>
+</html>
+
